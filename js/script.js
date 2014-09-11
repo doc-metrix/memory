f13fda251913b759e15a2a98264c31af0f9e393f
kgryte <kgryte@gmail.com>
2014-09-11 15:33:48 -0700

    [UPDATE] script.

--- js/script.js
+++ js/script.js
@@ -2,9 +2,7 @@
 (function() {
 	'use strict';
 
-	getResource( "https://api.github.com/repos/doc-metrix/memory/contents/spec/index.json", function onSpec( spec ) {
-		console.log( window.atob( JSON.parse( spec ).content ) );
-	});
+	getResource( "https://api.github.com/repos/doc-metrix/memory/contents/spec/index.json", onSpec );
 
 	// FUNCTIONS //
 
@@ -12,8 +10,9 @@
 	* FUNCTION: getResource( url, clbk )
 	*	Fetches a resource from a provided URL and returns the result to a provided callback.
 	*
-	* @param {string} url - resource location
-	* @param {function} clbk - callback to invoke upon resource receipt. Function should accept one input argument: [ result ]
+	* @private
+	* @param {String} url - resource location
+	* @param {Function} clbk - callback to invoke upon resource receipt. Function should accept one input argument: [ result ]
 	*/
 	function getResource( url, clbk ) {
 		var xhr;
@@ -35,5 +34,57 @@
 			xhr.send();
 		} // end IF (parameters)
 	} // end FUNCTION getResource()
+
+	/**
+	* FUNCTION: onSpec( blob )
+	*	Handler for receiving a spec resource.
+	*
+	* @private
+	* @param {String} blob - content blob which will be parsed as JSON
+	*/
+	function onSpec( blob ) {
+		var spec = window.atob( JSON.parse( blob ).content );
+		getFormulas( spec );
+	} // end FUNCTION onSpec()
+
+	/**
+	* FUNCTION: getFormulas( spec )
+	*	Extracts formulas from the specification and inserts them into the DOM.
+	*
+	* @private
+	* @param {Object} spec - metric specification
+	*/
+	function getFormulas( spec ) {
+		var keys = Object.keys( spec ),
+			cache = [],
+			name,
+			metric,
+			formula,
+			el;
+
+		// Slugify the keys, lowercase, and extract the formula:
+		for ( var i = 0; i < keys.length; i++ ) {
+			metric = spec[ keys[ i ] ];
+			formula = metric.formula;
+			if ( formula ) {
+				name = keys[ i ]
+					.split( '.' )
+					.join( '' )
+					.toLowerCase();
+				cache.push({
+					'name': name,
+					'formula': formula
+				});
+			}
+		} // end FOR i
+
+		// Use jQuery to select the table sibling following our metric of interest. We will insert the formula just after the table.
+		for ( var j = 0; j < cache.length; j++ ) {
+			name = cache[ j ].name;
+			el = $( 'a[name="'+name+'"]' ).next( 'table' );
+			el.before( '<p>Hello\n\n</p>' );
+		}
+
+	} // end FUNCTION getFormulas()
 
 })();
