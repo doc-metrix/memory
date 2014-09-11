38ca6990f81dd13529f90e41e348a799d5a4d9c9
kgryte <kgryte@gmail.com>
2014-09-11 14:43:21 -0700

    [UPDATE] include custom script. MathJax.

---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+(function() {
+	'use strict';
+
+	getResource( "https://raw.githubusercontent.com/doc-metrix/memory/master/spec/index.json", function onSpec( spec ) {
+		console.log( JSON.parse( spec ) );
+	});
+
+	// FUNCTIONS //
+
+	/**
+	* FUNCTION: getResource( url, clbk )
+	*	Fetches a resource from a provided URL and returns the result to a provided callback.
+	*
+	* @param {string} url - resource location
+	* @param {function} clbk - callback to invoke upon resource receipt. Function should accept one input argument: [ result ]
+	*/
+	function getResource( url, clbk ) {
+		var xhr;
+		if ( url && clbk ) {
+			// Create a new request object:
+			xhr = new XMLHttpRequest();
+
+			// Open the request connection:
+			xhr.open( 'GET', url );
+
+			// Define the state change callback:
+			xhr.onreadystatechange = function () {
+				if ( xhr.readyState != 4 || xhr.status != 200 ){
+					return;
+				}
+				clbk( xhr.responseText );
+			};
+			// Send the request:
+			xhr.send();
+		} // end IF (parameters)
+	} // end FUNCTION getResource()
+
+})();
