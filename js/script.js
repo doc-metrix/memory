fb4ff2e3da8ebac8d589d26d14ef4459628e6ed0
kgryte <kgryte@gmail.com>
2014-09-11 18:44:09 -0700

    [UPDATE] script run order.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 (function() {
 	'use strict';
 
-	getResource( "https://api.github.com/repos/doc-metrix/memory/contents/spec/index.json", onSpec );
+	getResource( 'https://api.github.com/repos/doc-metrix/memory/contents/README.md', onReadme );
 
 	// FUNCTIONS //
 
@@ -34,6 +34,41 @@
 			xhr.send();
 		} // end IF (parameters)
 	} // end FUNCTION getResource()
+
+	/**
+	* FUNCTION: onReadme( blob )
+	*	Handler for receiving a README resource.
+	*
+	* @private
+	* @param {String} blob - content blob which will be parsed as JSON
+	*/
+	function onReadme( blob ) {
+		var content = window.atob( JSON.parse( blob ).content );
+		render( content );
+	} // end FUNCTION onReadme()
+
+	/**
+	* FUNCTION: render( content )
+	*	Renders Markdown as HTML.
+	*
+	* @private
+	* @param {String} content - Markdown to render
+	*/
+	function render( content ) {
+		var html = marked( content );
+		document.getElementById( 'readme' ).innerHTML = html;
+		getSpec();
+	} // end FUNCTION render()
+
+	/**
+	* FUNCTION: getSpec()
+	*	Retrieves a metric specification.
+	*
+	* @private
+	*/
+	function getSpec() {
+		getResource( "https://api.github.com/repos/doc-metrix/memory/contents/spec/index.json", onSpec );
+	} // end FUNCTION getSpec()
 
 	/**
 	* FUNCTION: onSpec( blob )
